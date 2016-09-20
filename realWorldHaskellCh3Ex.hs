a6198b6f9d6835b2ffb8bdabec354e761448c8e2
LAIBOONH <LAIBOONH>
2016-09-20 21:03:08 +0800

    chapter3 exercise

---
+++ realWorldHaskellCh3Ex.hs
@@ -0,0 +1,9 @@
+{-# OPTIONS_GHC -Wall #-}
+
+count :: [a] -> Integer
+count (_:xs) = 1 + count xs
+count [] = 0
+
+count' :: Integer -> [a] -> Integer
+count' acc (_:xs) = count' (acc + 1) xs
+count' acc [] = acc
