046e4714aa6688a5facdab938b5a6f37b6eb65ac
lbh <laiboonh@gmail.com>
2016-09-20 09:40:25 +0800

    modified

--- realWorldHaskellCh3.hs
+++ realWorldHaskellCh3.hs
@@ -34,3 +34,45 @@ myBilling = Invoice 1
 
 -- How can we tell a BillingInfo is created by CreditCard CashOnDelivery or Invoice?
 -- Pattern Matching
+-- How can we tell a BillingInfo is created by CreditCard CashOnDelivery or Invoice?
+-- Pattern Matching
+sumList :: (Num a) => [a] -> a
+sumList (x:xs) = x + sumList(xs)
+sumList [] = 0
+
+-- Record syntax
+data Customer = Customer {
+  customerId        :: CustomerId
+  , customerName    :: String
+  , customerAddress :: Address
+} deriving Show
+
+myCustomer :: Customer
+myCustomer = Customer {
+  customerId = 1
+  ,customerName = "foo"
+  ,customerAddress = ["bar","baz"]
+}
+
+-- Parameterized Type
+-- a : type variable
+data Maybe' a = Just' a | Nothing' deriving Show
+someInt :: Maybe' Int
+someInt = Just' 1
+
+-- Recursive Type
+data List a = Cons a (List a) | Nil deriving Show
+myList :: List Int
+myList = Cons 1 (Cons 2 Nil)
+
+fromList :: [a] -> List a
+fromList (x:xs) = Cons x (fromList xs)
+fromList [] = Nil
+
+fromList' :: List a -> [a]
+fromList' (Cons x xs) = x : (fromList' xs)
+fromList' Nil = []
+
+data Tree a = Node a (Maybe (Tree a)) (Maybe (Tree a)) deriving Show
+myTree :: Tree Int
+myTree = Node 1 (Just(Node 2 (Nothing) (Nothing))) (Nothing)
