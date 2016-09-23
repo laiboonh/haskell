1f1ae0a3f71b13d538f05313baf75feb0cd9b71c
laiboonh <laiboonh@gmail.com>
2016-09-23 08:45:30 +0800

    More exercises

--- RealWorldHaskell/Chapter3/realWorldHaskellCh3Ex.hs
+++ RealWorldHaskell/Chapter3/realWorldHaskellCh3Ex.hs
@@ -7,3 +7,26 @@ count []     = 0
 count' :: Integer -> [a] -> Integer
 count' acc (_:xs) = count' (acc + 1) xs
 count' acc []     = acc
+
+sumList :: (Num a) => [a] -> a
+sumList [] = 0
+sumList (x:xs) = x + (sumList xs)
+
+lengthList :: (Num a) => [t] -> a
+lengthList [] = 0
+lengthList (_:xs) = 1 + (lengthList xs)
+
+meanList :: (Integral a, Fractional b) => [a] -> b
+meanList xs = fromIntegral(sumList xs) / (lengthList xs)
+
+reverseList :: [a] -> [a]
+reverseList (x:xs) = (reverseList xs) ++ [x]
+reverseList [] = []
+
+createPalin :: [a] -> [a]
+createPalin [] = []
+createPalin xs = xs ++ (reverseList xs)
+
+isPalin :: (Eq a) => [a] -> Bool
+isPalin [] = True
+isPalin xs = xs == (reverseList xs)
