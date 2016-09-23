d8c2b07fb28c018a4ece2c6ab44a86d1fdfe84ac
laiboonh <laiboonh@gmail.com>
2016-09-23 23:06:47 +0800

    README on PatternMatching

--- PatternMatching/README.md
+++ PatternMatching/README.md
@@ -8,3 +8,39 @@ firstLetter str@(x:_) = "The first letter of " ++ str ++ " is " ++ [x]
 ```
 
 ## guards
+```haskell
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | weight / height ^ 2 <= 18.5 = "You're underweight, you emo, you!"  
+    | weight / height ^ 2 <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | weight / height ^ 2 <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise                 = "You're a whale, congratulations!"  
+```
+
+## where bindings
+```haskell
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2
+```
+
+## let expressions
+```haskell
+cylinder :: (RealFloat a) => a -> a -> a  
+cylinder r h =
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea
+```
+
+## case expressions
+```haskell
+describeList :: [a] -> String  
+describeList xs = "The list is " ++ case xs of [] -> "empty."  
+                                               [x] -> "a singleton list."   
+                                               xs -> "a longer list."  
+```  
--- RealWorldHaskell/Chapter3/realWorldHaskellCh3Ex.hs
+++ RealWorldHaskell/Chapter3/realWorldHaskellCh3Ex.hs
@@ -1,6 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 
-import Data.List
+import           Data.List
 
 count :: [a] -> Integer
 count (_:xs) = 1 + count xs
@@ -10,12 +10,12 @@ count' :: Integer -> [a] -> Integer
 count' acc (_:xs) = count' (acc + 1) xs
 count' acc []     = acc
 
-sumList :: (Fractional a) => [a] -> a
-sumList [] = 0
+sumList :: (Num t) => [t] -> t
+sumList []     = 0
 sumList (x:xs) = x + (sumList xs)
 
-lengthList :: (Fractional a) => [t] -> a
-lengthList [] = 0
+lengthList :: (Num a) => [t] -> a
+lengthList []     = 0
 lengthList (_:xs) = 1 + (lengthList xs)
 
 meanList :: (Fractional a) => [a] -> a
@@ -23,7 +23,7 @@ meanList xs = (sumList xs) / (lengthList xs)
 
 reverseList :: [a] -> [a]
 reverseList (x:xs) = (reverseList xs) ++ [x]
-reverseList [] = []
+reverseList []     = []
 
 createPalin :: [a] -> [a]
 createPalin [] = []
@@ -47,6 +47,18 @@ compareByLength xs ys
     lengthys = length ys
 
 intersperse' :: a -> [[a]] -> [a]
-intersperse' _ [] = []
-intersperse' _ (x:[]) = x
+intersperse' _ []       = []
+intersperse' _ (x:[])   = x
 intersperse' sep (x:xs) = x ++ [sep] ++ intersperse' sep xs
+
+data Tree a = Node a (Tree a) (Tree a)
+            | Empty
+              deriving (Show)
+
+treeHeight :: Tree a -> Int
+treeHeight Empty            = 0
+treeHeight (Node _ (l) (r)) = 1 + (max (treeHeight l) (treeHeight r))
+
+simpleTree :: Tree [Char]
+simpleTree = Node "parent" (Node "left child" Empty Empty)
+                           (Node "right child" Empty Empty)
