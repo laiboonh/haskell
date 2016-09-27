b495cab8c268b441c832f5dc513219e73d3c8b01
laiboonh <laiboonh@gmail.com>
2016-09-27 22:28:49 +0800

    Cleanup

--- workspace.hs
+++ workspace.hs
@@ -1,7 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
 
-import Data.List (isInfixOf)
-
 -- To find out if lists in list is all same length
 sameLength :: [[a]] -> Bool
 sameLength [] = False
@@ -9,7 +7,3 @@ sameLength (x:xs) = foldl step True xs
   where
     headLen = length x
     step acc e = acc && (length e == headLen)
-
--- Lambda function
-isInAny :: String -> [String] -> Bool
-isInAny needle haystack = any (\s -> needle `isInfixOf` s) haystack
