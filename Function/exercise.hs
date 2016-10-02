9282d5884cf459895e2f07274b08fc7287047ee5
laiboonh <laiboonh@gmail.com>
2016-10-02 22:35:48 +0800

    some function exercises

--- Function/exercise.hs
+++ Function/exercise.hs
@@ -24,12 +24,12 @@ mflip f x y = f y x
 
 functionC :: Ord a => a -> a -> a
 functionC x y = case (x > y) of
-  True -> x
+  True  -> x
   False -> y
 
 ifEvenAdd2 :: Integral a => a -> a
 ifEvenAdd2 n = case (even n) of
-  True -> n + 2
+  True  -> n + 2
   False -> n
 
 num :: (Ord a, Num a) => a -> a
@@ -37,4 +37,22 @@ num x =
   case compare x 0 of
     LT -> -1
     GT -> 1
-    _ -> 0
+    _  -> 0
+
+foldBool :: a -> a -> Bool -> a
+foldBool x y b
+  | b == True = x
+  | otherwise = y
+
+foldBool' :: a -> a -> Bool -> a
+foldBool' x y b = case b of
+  True  -> x
+  False -> y
+
+tenDigit :: Integral a => a -> a
+tenDigit x = m
+    where dm = x `divMod` 100
+          m = snd dm
+
+g :: (a->b) -> (a,c) -> (b,c)
+g f (a,c) = (f a,c)
