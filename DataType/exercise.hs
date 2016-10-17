be18ed9cbf8976e76ad64aa7be7aea962bd7c5b9
lbh <laiboonh@gmail.com>
2016-10-17 22:33:23 +0800

    DataType exercises

--- DataType/exercise.hs
+++ DataType/exercise.hs
@@ -2,7 +2,7 @@
 
 module Cipher where
 
-import Data.Char
+import           Data.Char
 
 ordFirst :: Int
 ordFirst = ord 'A'
@@ -26,3 +26,41 @@ caesar _ [] _ = []
 caesar (x:xs) (y:ys) acc
   | x == ' ' = caesar xs (y:ys) ((shiftRight 0 x) : acc)
   | otherwise = caesar xs ys ((shiftRight (shiftAmt y) x) : acc)
+
+isSubsequenceOf :: (Eq a) => [a] -> [a] -> Bool
+isSubsequenceOf [] _ = True
+isSubsequenceOf _ [] = False
+isSubsequenceOf lxs@(x:xs) (y:ys)
+  | x == y = isSubsequenceOf xs ys
+  | otherwise = isSubsequenceOf lxs ys
+
+capitalizeWords :: String -> [(String,String)]
+capitalizeWords "" = []
+capitalizeWords xs = map mkTuple (words xs)
+  where mkTuple wd@(c:cs) = (wd, (toUpper c):cs)
+        mkTuple [] = error "mkTuple empty string"
+
+capitalizeWord :: String -> String
+capitalizeWord [] = []
+capitalizeWord (x:xs) = (toUpper x):xs
+
+capitalizeParagraph :: String -> String
+capitalizeParagraph [] = []
+capitalizeParagraph xs = unsentence $ map capitalizeSentence (sentence xs)
+
+sentence :: String -> [String]
+sentence xs = reverse $ go [] xs
+  where go :: [String] -> String -> [String]
+        go acc [] = acc
+        go acc xs' = go (first : acc) rest
+                     where first = takeWhile (/='.') xs
+                           rest = drop 2 (dropWhile (/='.') xs')
+
+unsentence :: [String] -> String
+unsentence [] = []
+unsentence xs = init $ foldr (\x acc -> x ++ ". " ++ acc) [] xs
+
+capitalizeSentence :: String -> String
+capitalizeSentence [] = []
+capitalizeSentence xs = (capitalizeWord (head w)) ++ (' ' : (unwords $ tail w))
+  where w = words xs
