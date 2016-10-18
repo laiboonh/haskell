74b2111c7d04e6d7ca84ca25817b95a3a378a704
lbh <laiboonh@gmail.com>
2016-10-18 23:26:11 +0800

    DataType exercises

--- DataType/exercise.hs
+++ DataType/exercise.hs
@@ -3,6 +3,7 @@
 module Cipher where
 
 import           Data.Char
+import Data.List (findIndex, group, nub, sort)
 
 ordFirst :: Int
 ordFirst = ord 'A'
@@ -64,3 +65,78 @@ capitalizeSentence :: String -> String
 capitalizeSentence [] = []
 capitalizeSentence xs = (capitalizeWord (head w)) ++ (' ' : (unwords $ tail w))
   where w = words xs
+
+myPhone :: Phone
+myPhone =
+  [
+    ['1'],
+    ['a','b','c','2'],
+    ['d','e','f','3'],
+    ['g','h','i','4'],
+    ['j','k','l','5'],
+    ['m','n','o','6'],
+    ['p','q','r','s','7'],
+    ['t','u','v','8'],
+    ['w','x','y','z','9'],
+    ['+', ' ', '0'],
+    ['^', '*'],
+    ['.',',','#']
+  ]
+
+type Phone = [Keys]
+type Keys = [Char]
+type Digit = Char
+type Presses = Int
+
+reverseTaps :: Phone -> Char -> [(Digit,Presses)]
+reverseTaps [] c = error ("Illegal Digit " ++ [c])
+reverseTaps (k:ks) c =
+  case findIndex (==lowC) k of
+    Just i -> if (toUpper c == c) then [('*',1),((last k),(i+1))] else [((last k),(i+1))]
+    Nothing -> reverseTaps ks c
+  where lowC = toLower c
+
+cellPhonesDead :: Phone -> String -> [(Digit,Presses)]
+cellPhonesDead p s = foldr ((++) . (reverseTaps p)) [] s
+
+fingerTaps :: [(Digit,Presses)] -> Presses
+fingerTaps [] = 0
+fingerTaps xs = foldr ((+) . snd) 0 xs
+
+mostPopularLetter :: String -> Char
+mostPopularLetter "" = error "Empty String"
+mostPopularLetter s =
+  case findIndex (==maxCount) counts of
+    Just i -> chars !! i
+    Nothing -> error "Not possible"
+    where groupedStr = group $ sort $ map toLower s
+          counts = map length groupedStr
+          chars = map (head .nub) groupedStr
+          maxCount = maximum counts
+
+convo :: [String]
+convo =
+    ["Wanna play 20 questions",
+     "Ya",
+     "U 1st haha",
+     "Lol ok. Have u ever tasted alcohol lol",
+     "Lol ya",
+     "Wow ur cool haha. Ur turn",
+     "Ok. Do u think I am pretty Lol",
+     "Lol ya",
+     "Haha thanks just making sure rofl ur turn"]
+
+coolestLtr :: [String] -> Char
+coolestLtr = mostPopularLetter . concat . concat . (map words)
+
+data Expr =
+  Lit Integer
+  | Add Expr Expr
+
+eval :: Expr -> Integer
+eval (Lit i) = i
+eval (Add e1 e2) = (eval e1) + (eval e2)
+
+printExpr :: Expr -> String
+printExpr (Lit i) = show i
+printExpr (Add e1 e2) = printExpr e1 ++ " + " ++ printExpr e2
