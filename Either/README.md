05f3908757127327e028274fd8da92b620e437ee
lbh <laiboonh@gmail.com>
2016-10-19 14:36:02 +0800

    more notes on Either

--- Either/README.md
+++ Either/README.md
@@ -22,5 +22,24 @@ mkPerson' name age
 ```
 #### improvement
 ```haskell
+type ValidatePerson a = Either [PersonInvalid] a
 
+ageOkay :: Age -> Either [PersonInvalid] Age
+ageOkay age = case age >=0 of
+  True -> Right age
+  False -> Left [AgeTooLow]
+
+nameOkay :: Name -> Either [PersonInvalid] Name
+nameOkay name = case name /= "" of
+  True -> Right name
+  False -> Left [NameEmpty]
+
+mkPerson :: Name -> Age -> ValidatePerson Person
+mkPerson name age = mkPerson' (nameOkay name) (ageOkay age)
+
+mkPerson' :: ValidatePerson Name -> ValidatePerson Age -> ValidatePerson Person
+mkPerson' (Right nameOk) (Right ageOk) = Right (Person nameOk ageOk)
+mkPerson' (Left badName) (Left badAge) = Left (badName ++ badAge)
+mkPerson' (Left badName) _ = Left badName
+mkPerson' _ (Left badAge) = Left badAge
 ```
--- Either/workspace.hs
+++ Either/workspace.hs
@@ -5,15 +5,27 @@ type Age = Integer
 
 data Person = Person Name Age deriving Show
 
-mkPerson :: Name -> Age -> Maybe Person
-mkPerson name age
-  | name /= "" && age >=0 = Just $ Person name age
-  | otherwise = Nothing
+
 
 data PersonInvalid = NameEmpty | AgeTooLow deriving (Eq,Show)
 
-mkPerson' :: Name -> Age -> Either PersonInvalid Person
-mkPerson' name age
-  | name /= "" && age >=0 = Right $ Person name age
-  | name == "" = Left NameEmpty
-  | otherwise = Left AgeTooLow
+type ValidatePerson a = Either [PersonInvalid] a
+
+ageOkay :: Age -> Either [PersonInvalid] Age
+ageOkay age = case age >=0 of
+  True -> Right age
+  False -> Left [AgeTooLow]
+
+nameOkay :: Name -> Either [PersonInvalid] Name
+nameOkay name = case name /= "" of
+  True -> Right name
+  False -> Left [NameEmpty]
+
+mkPerson :: Name -> Age -> ValidatePerson Person
+mkPerson name age = mkPerson' (nameOkay name) (ageOkay age)
+
+mkPerson' :: ValidatePerson Name -> ValidatePerson Age -> ValidatePerson Person
+mkPerson' (Right nameOk) (Right ageOk) = Right (Person nameOk ageOk)
+mkPerson' (Left badName) (Left badAge) = Left (badName ++ badAge)
+mkPerson' (Left badName) _ = Left badName
+mkPerson' _ (Left badAge) = Left badAge
