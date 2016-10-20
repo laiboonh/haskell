8b00e8b125491b2b19340dacde12073c2dd84996
lbh <laiboonh@gmail.com>
2016-10-20 18:41:38 +0800

    either exercies

--- Either/exercise.hs
+++ Either/exercise.hs
@@ -64,3 +64,42 @@ integerToNat i
           go 0 = Zero
           go 1 = Succ Zero
           go x = Succ (go (x-1))
+
+isJust :: Maybe a -> Bool
+isJust Nothing = False
+isJust (Just _) = True
+
+isNothing :: Maybe a -> Bool
+isNothing m = not $ isJust m
+
+mayybee :: b -> (a->b) -> Maybe a -> b
+mayybee x _ (Nothing) = x
+mayybee _ f (Just a) = f a
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe x Nothing = x
+fromMaybe _ (Just a) = a
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe [] = Nothing
+listToMaybe (x:_) = Just x
+
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing = []
+maybeToList (Just a) = [a]
+
+catMaybes :: [Maybe a] -> [a]
+catMaybes xs = foldr f [] xs
+  where f :: Maybe a -> [a] -> [a]
+        f Nothing acc = acc
+        f (Just a) acc = a:acc
+
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe [] = Nothing
+flipMaybe lst = case go lst of
+  [] -> Nothing
+  ys -> Just ys
+  where go :: [Maybe a] -> [a]
+        go [] = []
+        go (Nothing:xs) = go xs
+        go ((Just a):xs) = a : (go xs)
