854fa62f7790894ca7d8ee2d7ec2eda249ca91a7
lbh <laiboonh@gmail.com>
2016-10-22 10:13:41 +0800

    Hangman complete

--- Cabal/hangman/src/Main.hs
+++ Cabal/hangman/src/Main.hs
@@ -1,18 +1,19 @@
 module Main where
 
-import Control.Monad (forever)
-import Data.Char (toLower)
-import Data.Maybe (isJust)
-import Data.List (intersperse)
-import System.Exit (exitSuccess)
-import System.Random (randomRIO)
+import           Control.Monad (forever)
+import           Data.Char     (toLower)
+import           Data.List     (intersperse)
+import           Data.Maybe    (isJust, isNothing)
+import           System.Exit   (exitSuccess)
+import           System.Random (randomRIO)
 
-type WordList = [String]
+--type WordList = [String]
+newtype WordList = WordList [String] deriving (Eq,Show)
 
 allWords :: IO WordList
 allWords = do
   dict <- readFile "data/words"
-  return (lines dict)
+  return $ WordList (lines dict)
 
 minWordLength :: Int
 minWordLength = 5
@@ -22,18 +23,97 @@ maxWordLength = 9
 
 gameWords :: IO WordList
 gameWords = do
-  aw <- allWords
-  return (filter gameLength aw)
-  where gameLength w = let l = length w
-                       in l > minWordLength && l < maxWordLength
+  (WordList aw) <- allWords
+  return $ WordList (filter gameLength aw)
+  where gameLength w =
+              let l = length w
+              in l > minWordLength && l < maxWordLength
 
 randomWord ::WordList -> IO String
-randomWord wl = do
+randomWord (WordList wl) = do
   randomIndex <- randomRIO (0,(length wl)-1)
   return (wl !! randomIndex)
 
 randomWord' :: IO String
 randomWord' = gameWords >>= randomWord
 
+data Puzzle = Puzzle String [Maybe Char] [Char]
+
+instance Show Puzzle where
+  show (Puzzle _ discovered guessed) =
+    (intersperse ' ' $ fmap renderPuzzleChar discovered)
+    ++ "Guessed so far: " ++ guessed
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle word = Puzzle word (replicate (length word) Nothing) []
+
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle word _ _) c = elem c word
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ guessed) c = elem c guessed
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar Nothing = '_'
+renderPuzzleChar (Just c)= c
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar s) c =
+  Puzzle word newFilledInSoFar newGuessedSoFar
+  where zipper guessed wordChar guessChar =
+          if wordChar == guessed
+          then Just wordChar
+          else guessChar
+        newFilledInSoFar =
+          zipWith (zipper c) word filledInSoFar
+        newGuessedSoFar
+          | elem c word = s
+          | otherwise = c:s
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+  putStrLn $ "Your guess was: " ++ [guess]
+  case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+    (_,True) -> do
+      putStrLn "You already guess that character, pick something else!"
+      return puzzle
+    (True,_) -> do
+      putStrLn "This character was in the word, filling in the word accordingly"
+      return (fillInCharacter puzzle guess)
+    (False,_) -> do
+      putStrLn "This character wasn't in the word, try again."
+      return (fillInCharacter puzzle guess)
+
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess discovered guessed) =
+  if (length guessed > maxWordLength && (any isNothing discovered)) then
+    do
+      putStrLn "You lose!"
+      putStrLn $ "The word was: " ++ wordToGuess
+      exitSuccess
+  else return()
+
+gameWin :: Puzzle -> IO ()
+gameWin (Puzzle _ filledInSoFar _) =
+  if all isJust filledInSoFar then
+    do
+      putStrLn "You win!"
+      exitSuccess
+    else return ()
+
+runGame :: Puzzle -> IO ()
+runGame puzzle = forever $ do
+  gameOver puzzle
+  gameWin puzzle
+  putStrLn $ "Current puzzle is: " ++ show puzzle
+  putStr "Guess a letter: "
+  guess <- getLine
+  case guess of
+    [c] -> handleGuess puzzle c >>= runGame
+    _ -> putStrLn "Your guess must be a single character"
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  word <- randomWord'
+  let puzzle = freshPuzzle (fmap toLower word)
+  runGame puzzle
