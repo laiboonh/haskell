e1939dc2bf24339adb74df1b7f5482818c212a3b
lbh <laiboonh@gmail.com>
2016-10-25 09:43:21 +0800

    some mooned exercise

--- Monoid/workspace.hs
+++ Monoid/workspace.hs
@@ -11,3 +11,16 @@ instance Monoid a => Monoid (Optional a) where
   mappend (Only _) Nada = Nada
   mappend Nada Nada = Nada
   mappend (Only a1) (Only a2) = Only (mappend a1 a2)
+
+
+type Exclamation = String
+type Adverb = String
+type Noun = String
+type Adjective = String
+
+madLibbinBetter :: Exclamation
+                   -> Adverb
+                   -> Noun
+                   -> Adjective
+                   -> String
+madLibbinBetter e adv noun adj = mconcat [e,"! he said ",adv," as he jumped into his car ",noun," and drove off with this ",adj, " wife."]
