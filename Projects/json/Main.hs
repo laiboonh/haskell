254b402b48807dca10fb5e399e0c140e24991204
lbh <laiboonh@gmail.com>
2016-12-04 09:28:17 +0800

    json final commit

--- Projects/json/Main.hs
+++ Projects/json/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
+import           PutJSON
 import           SimpleJSON
 
 main :: IO ()
-main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+main = putJValue (JObject [("foo", JNumber 1), ("bar", JBool False)])
